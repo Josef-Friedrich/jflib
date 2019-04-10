06be98d8a12da0cc7119d2d4b01cb90fb01541b6
Josef Friedrich <josef@friedrich.rocks>
2019-04-10 21:25:19 +0200

    Add support for versioneer and travis deploy

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 import os
 
+import versioneer
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -10,7 +12,9 @@ setup(
     name='jflib',
     author='Josef Friedrich',
     author_email='josef@friedrich.rocks',
-    description=('A collection of my Python scripts. Maybe they are useful for someone else.'),
+    description=('A collection of my Python library snippets. Maybe they are useful for someone else.'),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     license='MIT',
     project_urls={
         'Documentation': 'http://jflib.readthedocs.io/en/latest/',
@@ -18,7 +22,7 @@ setup(
         'Tracker': 'https://github.com/Josef-Friedrich/jflib/issues',
     },
     packages=['jflib'],
-    url='https://github.com/Josef-Friedrich/python-scripts',
+    url='https://github.com/Josef-Friedrich/jflib',
     python_requires='>=3.6',
     long_description=read('README.md'),
     classifiers=[
