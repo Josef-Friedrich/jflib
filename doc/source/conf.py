9dc7c2a3aac355716195be77ab58ffe338001d8b
Josef Friedrich <josef@friedrich.rocks>
2019-04-15 16:21:08 +0200

    Add doc support

---
+++ doc/source/conf.py
@@ -0,0 +1,35 @@
+import sphinx_rtd_theme
+import jflib
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+extensions = []
+extensions += ['sphinx.ext.autodoc']
+extensions += ['sphinx.ext.intersphinx']
+extensions += ['sphinx.ext.viewcode']
+extensions += ['sphinxarg.ext']
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = u'jflib'
+copyright = u'2019, Josef Friedrich'
+author = u'Josef Friedrich'
+version = jflib.__version__
+release = jflib.__version__
+language = None
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_static_path = []
+htmlhelp_basename = 'jflibdoc'
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'show-inheritance': True,
+}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
---
+++ doc/source/index.rst
@@ -0,0 +1,18 @@
+#################################
+Welcome to jflib's documentation!
+#################################
+
+.. automodule:: jflib.argparser_to_readme
+.. automodule:: jflib.capturing
+.. automodule:: jflib.command_watcher
+.. automodule:: jflib.send_email
+.. automodule:: jflib.termcolor
+
+******************
+Indices and tables
+******************
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
